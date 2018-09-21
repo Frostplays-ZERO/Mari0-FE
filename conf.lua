2e9ef5ff159524eb36a21388c1c82814dccd9edb
Frostplays-ZERO <43028444+Frostplays-ZERO@users.noreply.github.com>
2018-09-21 19:51:22 +1000

    Added all the lua so far
    Initial commit

---
+++ conf.lua
@@ -0,0 +1,9 @@
+function love.conf(t)
+	t.title = "Mari0 FE"
+	t.author = "Maurice"
+	t.identity = "mari0_fe"
+	t.modules.physics = false
+	t.release = false
+	t.window = false
+	t.console = true
+end
